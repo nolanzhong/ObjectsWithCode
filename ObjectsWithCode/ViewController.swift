54400ee4acb68b4a6d7b85cd90a5bcd44553cd57
nolanzhong <nolan.zhong@duke.edu>
2022-12-20 22:39:35 -0500

    placed button in direct middle of screen

--- ObjectsWithCode/ViewController.swift
+++ ObjectsWithCode/ViewController.swift
@@ -12,11 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //width and height of phone
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        
         let myLabel = UILabel()
         myLabel.text = "Test Label"
         myLabel.textAlignment = .center
-        // frame specifies location and size
-        myLabel.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        // frame specifies location and size (placing label in direct center)
+        myLabel.frame = CGRect(x: width * 0.5 - width * 0.8 / 2, y: height * 0.5, width: width * 0.8 - 50/2, height: 50)
         view.addSubview(myLabel)
     }
 
