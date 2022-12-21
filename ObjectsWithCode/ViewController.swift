06ee9c163288dee2489b3adba6fa3e2e04f8626b
nolanzhong <nolan.zhong@duke.edu>
2022-12-20 22:50:06 -0500

    created button functions

--- ObjectsWithCode/ViewController.swift
+++ ObjectsWithCode/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // created this variable in class so can be accessible in function
+    var myLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,14 +20,26 @@ class ViewController: UIViewController {
         let width = view.frame.size.width
         let height = view.frame.size.height
         
-        let myLabel = UILabel()
+        
         myLabel.text = "Test Label"
         myLabel.textAlignment = .center
         // frame specifies location and size (placing label in direct center)
         myLabel.frame = CGRect(x: width * 0.5 - width * 0.8 / 2, y: height * 0.5, width: width * 0.8 - 50/2, height: 50)
         view.addSubview(myLabel)
+        
+        //creating button
+        let myButton = UIButton()
+        myButton.setTitle("My Button", for: UIControl.State.normal)
+        myButton.setTitleColor(UIColor.cyan, for: UIControl.State.normal)
+        myButton.frame = CGRect(x: width*0.5 - 100, y: height*0.6, width: 200, height: 200)
+        view.addSubview(myButton)
+        
+        myButton.addTarget(self, action: #selector(ViewController.myAction), for: UIControl.Event.touchUpInside) // means do action if touching the button
     }
-
+    // creating an objective c function for selctor
+    @objc func myAction() {
+        myLabel.text = "Tapped"
+    }
 
 }
 
