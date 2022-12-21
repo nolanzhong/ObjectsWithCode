376990723ec14e98d71d2283ccd926e9b8e7e331
nolanzhong <nolan.zhong@duke.edu>
2022-12-20 22:31:04 -0500

    added basic label with code

--- ObjectsWithCode/ViewController.swift
+++ ObjectsWithCode/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let myLabel = UILabel()
+        myLabel.text = "Test Label"
+        myLabel.textAlignment = .center
+        // frame specifies location and size
+        myLabel.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        view.addSubview(myLabel)
     }
 
 
